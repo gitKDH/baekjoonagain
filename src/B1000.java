4b2429966c6cadb7b8fb664e95b5359c02d79a7d
DongHyun <donghyun@DongHyunui-MacBookAir.local>
2025-01-07 20:25:41 +0900

    Initial commit

---
+++ src/B1000.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class B1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        int b;
+        int sum;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        sum = a + b;
+        System.out.println(sum);
+    }
+}
